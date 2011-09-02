b334082533ec5fafd10b563ec04862b65fe0a65a
Ben <benswanson1@gmail.com>
2011-09-02 22:05:03 +0100

    Awesome Update

--- Assets/stardestroyermove.js
+++ Assets/stardestroyermove.js
@@ -1,6 +1,14 @@
-
 function Update () {
 
-transform.Translate(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+	transform.Translate(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+ 	/*transform.Rotate(Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * Time.deltaTime * speed);*/
+ 	
+ 	
+    // Get the mouse delta. This is not in the range -1...1
+    var h : float = 2 * Input.GetAxis ("Mouse X");
+    var v : float = 2 * Input.GetAxis ("Mouse Y");
+    
+    transform.Rotate (v, h, 0);
+    
 
 }
